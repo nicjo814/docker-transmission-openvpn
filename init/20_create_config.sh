1aaa09c542e8db1425783e62bc4528d9ad7250cb
Niclas Berglind <nb@kjam.se>
2015-09-11 06:36:12 +0200

    New init script for OpenVPN

--- init/20_create_config.sh
+++ init/20_create_config.sh
@@ -3,9 +3,3 @@
 if [ ! -f /config/settings.json ]; then
     cp /etc/transmission/settings.json /config/settings.json
 fi
-
-if [ ! -f /config/openvpn.ovpn ]; then
-    export OPENVPNCFG="False"
-else
-    export OPENVPNCFG="True"
-fi
