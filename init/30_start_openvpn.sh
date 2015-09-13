ee64299fa1d32c4111ffc229520112335ff8ec56
Niclas Berglind <nb@kjam.se>
2015-09-13 20:51:12 +0200

    Copy resolv.conf on startup

--- init/30_start_openvpn.sh
+++ init/30_start_openvpn.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 if [ -f /config/openvpn.ovpn ]; then
+    cp /config/resolv.conf /etc/resolv.conf
     mkdir -p /dev/net
     if [ ! -c /dev/net/tun ]; then
         mknod /dev/net/tun c 10 200
